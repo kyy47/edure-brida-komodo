39377444c3ba90ebc474e7979f2ce07278e6a85b
kyy47 <ourbaerapy@gmail.com>
2023-12-27 21:31:11 +0800

    fix: snackbar

--- src/hooks/useSnackbar.tsx
+++ src/hooks/useSnackbar.tsx
@@ -18,7 +18,7 @@ export default function useSnackbar() {
   function Snackbar() {
     if (!toggle) return null;
     return (
-      <div className="absolute top-0 left-0 flex justify-center w-full items-center z-[99999]">
+      <div className="fixed top-0 left-0 flex justify-center w-full items-center z-[99999]">
         <div className="bg-cranberry-100 w-fit mt-5 px-8 py-2 rounded-lg text-gray-600 flex items-center gap-2 ">
           {iconSnackbar === "loading" ? (
             <span className="w-4 h-4 border-2 border-black border-t-cranberry-300 border-b-cranberry-500 rounded-full animate-spin" />
